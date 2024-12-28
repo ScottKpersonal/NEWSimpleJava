f6e063c1be355e106cef953377e9b3502d89a460
Scott K <kredoscott@hotmail.com>
2024-12-29 00:36:56 +0200

    Second commit while PR is open on the branch

--- Simplejava.java
+++ Simplejava.java
@@ -1,5 +1,6 @@
 //The simplest of code for testing
 public class SimpleJava {
+    /** Main method that prints "First Branch First Change" to console */
     public static void main(String[] args) {
         System.out.println("First Branch First Change");
     }
